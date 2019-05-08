b56a05b2d89af17a7ac5d34c69ea3832f32f3dd7
Philip Middleton <middleton.philip.pm@gmail.com>
2019-05-08 00:21:42 -0400

    Added entry code

---
+++ src/scripts/main.js
@@ -0,0 +1,9 @@
+const loremEl = document.querySelector('.lorem');
+
+const getData = () => {
+  return fetch('https://baconipsum.com/api/?type=all-meat&paras=3')
+    .then(res => res.json())
+    .then(res => {
+      loremEl.innerHTML = res
+    });
+};
---
+++ webpack.config.js
@@ -0,0 +1,99 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const isDevelopment = process.env.NODE_ENV === 'development';
+
+const htmlWebpackPlugin = new HtmlWebpackPlugin({
+  template: path.resolve(__dirname, 'src', 'index.html'),
+  filename: 'index.html',
+  title: 'Vanilla-AF',
+});
+
+module.exports = {
+  entry: {
+    index: './src/scripts/main.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    filename: isDevelopment 
+      ? '[name].bundle.js'
+      : '[name].bundle-[hash:6].js',
+    chunkFilename: isDevelopment
+      ? '[name].bundle.js'
+      : '[name].bundle-[hash:6].js',
+  },
+  devtool: isDevelopment
+    ? 'eval-source-map'
+    : 'source-map',
+  devServer: {
+    inline: true,
+    contentBase: path.resolve(__dirname, 'public'),
+    port: 9669,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(eot|otf|ttf|woff|woff2)$/,
+        loader: 'file?name=public/fonts/[name].[ext]'
+      },
+      {
+        test: /\.svg$/,
+        loader: 'file-loader',
+        query: {
+          name: 'static/media/[name].[hash:8].[ext]'
+        }
+      }
+    ],
+  },
+  plugins: [
+    htmlWebpackPlugin,
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+      },
+    }),
+  ],
+  resolve: {
+    modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+    extensions: ['.js', '.json'],
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        node_vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          chunks: 'all',
+          priority: 1
+        }
+      }
+    },
+  }
+};
